929bd09eb79767b226247ef2c44c6801cd5b0ad9
GitHub Action <action@github.com>
2021-02-27 07:35:47 +0000

    Template cleanup

--- src/main/kotlin/com/github/phoenixxc/customfolding/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/phoenixxc/customfolding/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.phoenixxc.customfolding.listeners
 
+import com.github.phoenixxc.customfolding.services.MyProjectService
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
--- src/main/kotlin/com/github/phoenixxc/customfolding/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/phoenixxc/customfolding/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.phoenixxc.customfolding.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.phoenixxc.customfolding.MyBundle
 
 class MyApplicationService {
 
